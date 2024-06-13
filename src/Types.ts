fe71146b863c983a0bfd55b8df7d3f72edb9c239
Daniel Maioni Araújo de Assis <sniper_dmaa@hotmail.com>
2024-06-13 13:30:13 +0100

    Types

---
+++ src/Types.ts
@@ -0,0 +1,31 @@
+
+
+let abc = undefined;
+const def = null;
+
+function getData(): string | undefined {
+    return '';
+}
+
+const data = getData();
+
+if (data) {
+    const someOtherData = data;  
+} 
+
+let input: unknown;
+input = 'someInput';
+let someSensitiveValue: string;
+
+if (typeof input === 'string') {
+    someSensitiveValue = input;
+}
+
+function doTask(tasks: number): void | never {
+    if (tasks > 3) {
+        throw new Error('Too many tasks');
+    } 
+}
+
+const stuff = doTask(2);
+
